6ef1661f6ee312b35a38e9096a58a2fd0e2f2643
Christian Kaltepoth <christian@kaltepoth.de>
2015-09-10 07:41:18 +0200

    Added LESS support and refactored gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,37 @@ var gulp = require("gulp");
 var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
+var less = require('gulp-less');
+var rename = require("gulp-rename");
 
-gulp.task("default", function () {
-  return gulp.src("./src/main/webapp/**/*.js")
+var paths = {
+  js: "./src/main/webapp/**/*.js",
+  css: "./src/main/webapp/**/*.less",
+  out: "./target/es6-webapp/"
+};
+
+gulp.task("js", function () {
+  gulp.src(paths.js)
     .pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(rename(function (path) {
+      path.basename = path.basename.replace(/\.es6/, "");
+    }))
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest("./target/es6-webapp/"));
+    .pipe(gulp.dest(paths.out));
 });
+
+gulp.task("css", function () {
+  gulp.src(paths.css)
+    .pipe(less())
+    .pipe(gulp.dest(paths.out));
+});
+
+gulp.task("watch", function () {
+  gulp.watch(paths.js, ["js"]);
+  gulp.watch(paths.css, ["css"]);
+});
+
+gulp.task("default", ["js", "css"]);
+
