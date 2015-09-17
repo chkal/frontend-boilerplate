24b13247b4dc822393e64527ad13ae7b262bb64a
Christian Kaltepoth <christian@kaltepoth.de>
2015-09-17 07:57:48 +0200

    Fixed relative path issue in watch scripts

--- scripts/watch-eclipse.cmd
+++ scripts/watch-eclipse.cmd
@@ -1,3 +1,3 @@
 @echo off
-%~dp0../node/node ../node_modules/gulp/bin/gulp.js watch --dest ../m2e-wtp/web-resources/
+%~dp0../node/node ../node_modules/gulp/bin/gulp.js watch --dest m2e-wtp/web-resources/
 @echo on
--- scripts/watch-eclipse.sh
+++ scripts/watch-eclipse.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-../node/node ../node_modules/gulp/bin/gulp.js watch --dest ../m2e-wtp/web-resources/
+../node/node ../node_modules/gulp/bin/gulp.js watch --dest m2e-wtp/web-resources/
