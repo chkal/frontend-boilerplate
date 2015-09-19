e76463f456d6a2a2c87bb9b9c5d54e568b2fd74f
Christian Kaltepoth <christian@kaltepoth.de>
2015-09-19 08:23:20 +0200

    Revert "Show how to include 3rd party libs"
    This reverts commit ec16a108588f7422b6020337ec057f4d922dd5fa.

--- src/main/javascript/index.entry.js
+++ src/main/javascript/index.entry.js
@@ -1,12 +1,10 @@
 import Person from "./lib/person.js";
 import template from "./template.hb";
-import moment from "moment";
 
 const person = new Person("Christian");
 
 document.getElementById("root").innerHTML = template({
-  greeting: person.greet(),
-  date: moment().format("YYYY-MM-DD" )
+  greeting: person.greet()
 });
 
 console.log("Done!");
