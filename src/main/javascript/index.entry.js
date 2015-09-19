ec16a108588f7422b6020337ec057f4d922dd5fa
Christian Kaltepoth <christian@kaltepoth.de>
2015-09-19 08:12:26 +0200

    Show how to include 3rd party libs

--- src/main/javascript/index.entry.js
+++ src/main/javascript/index.entry.js
@@ -1,10 +1,12 @@
 import Person from "./lib/person.js";
 import template from "./template.hb";
+import moment from "moment";
 
 const person = new Person("Christian");
 
 document.getElementById("root").innerHTML = template({
-  greeting: person.greet()
+  greeting: person.greet(),
+  date: moment().format("YYYY-MM-DD" )
 });
 
 console.log("Done!");
--- src/main/javascript/template.hb
+++ src/main/javascript/template.hb
@@ -1,2 +1,3 @@
 <h1>Frontend Boilerplate</h1>
 <h2>{{greeting}}</h2>
+<h2>Today: {{date}}<h2>
