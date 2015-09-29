029ba6ade7169ddaab5fa9d59269f0f2572a6601
Christian Kaltepoth <christian@kaltepoth.de>
2015-09-29 17:37:33 +0200

    Use phantomjs for unit tests by default...

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ["Chrome"],
+    browsers: ["PhantomJS"],
     singleRun: true
   });
 
