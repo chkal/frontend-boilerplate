2f1fa0704ef399ae439063e329d208d81c659c91
Christian Kaltepoth <christian@kaltepoth.de>
2015-11-27 17:46:02 +0100

    Removed gulp

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var argv = require('yargs').argv;
+
+if (!argv.dest) {
+  throw Error("Please use --dest to set a target directory");
+}
+
+module.exports = {
+  entry: {
+    index: "./src/main/javascript/index.entry.js"
+  },
+  output: {
+    path: path.join(argv.dest, "js"),
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel"
+    }, {
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      loader: "ts-loader"
+    }, {
+      test: /\.hb$/,
+      exclude: /node_modules/,
+      loader: "handlebars-loader"
+    }]
+  }
+};
