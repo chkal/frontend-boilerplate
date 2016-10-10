26e9f0cfdeb982169e6751e65bfcc49034df7b40
Christian Kaltepoth <christian@kaltepoth.de>
2016-10-10 18:07:42 +0200

    Test should be written in TypeScript

--- src/main/javascript/index.js
+++ src/main/javascript/index.js
@@ -2,6 +2,6 @@ import Person from "lib/person.ts";
 
 const person = new Person("Christian");
 
-document.getElementById("root").innerHTML = "Hello: " + person.greet();
+document.getElementById("root").innerHTML = person.greet();
 
 console.log("Done!");
